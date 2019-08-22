23df6d53410652860e8df83adacea31e68b10a20
Rudenko Ivan <rudenko_ia@outlook.com>
2019-08-22 20:56:31 +0900

    fix list action

--- views/activity/list.php
+++ views/activity/list.php
@@ -12,7 +12,7 @@
 <?php foreach ($activities as $activity): ?>
         <tr>
             <td><?= $activity->title ?></td>
-            <td></td>
+            <td><?= $activity->dateStart ?></td>
             <td><a href="/activity/view?id=<?=$activity->id?>">view</a> | <a href="/activity/edit?id=<?=$activity->id?>"">edit</a></td>
         </tr>
 <?php endforeach; ?>
